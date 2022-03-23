385c2537c6791da60386d527d68f6b96d8b66dd4
wowBlaBla <wow1992122@gmail.com>
2022-03-23 12:06:34 +0900

    change the output file from .txt to .json

--- main.go
+++ main.go
@@ -6,9 +6,8 @@ import (
 	"time"
 )
 
-var file *os.File
-
 func main() {
+	fmt.Printf("\n@@@ Start Time: %s\n\n", time.Now().String())
 	loadConfig(&config)
 
 	// get the new CID from pending tx, if config.UpdateCID is true
@@ -26,44 +25,18 @@ func main() {
 		updateConfigFile()
 	}
 
-	// initialize the variables
-	total_cnt := config.Ipfs_info.Total_Count
-	thread_cnt := config.Ipfs_info.Thread_Count
-	ch_result := make(chan bool, thread_cnt)
-
 	// open the metadata.txt file
-	var err error
-	file, err = os.OpenFile("./output/metadata.txt", os.O_APPEND|os.O_WRONLY, 0644)
+	file, err := os.OpenFile("./output/metadata.json", os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// write the current time to metadata.txt file
-	str_time := time.Now().String()
-	file.WriteString("\n-------------------- " + str_time + " --------------------\n")
-
-	// generate multithread for requesting the metadata from IPFS
-	req_cnt := total_cnt / thread_cnt
-	for i := 0; i < thread_cnt; i++ {
-		if i < thread_cnt-1 {
-			go getMetadata(config.Ipfs_info.Start_Index+i*10, req_cnt, ch_result)
-		} else {
-			go getMetadata(config.Ipfs_info.Start_Index+i*10, req_cnt+(total_cnt%thread_cnt), ch_result)
-		}
-	}
-
-	// parse and write the metadata to file
-	for i := 0; i < thread_cnt; i++ {
-		<-ch_result
-	}
-
-	// write the current time to metadata.txt file
-	str_time = time.Now().String()
-	file.WriteString("-------------------- " + str_time + " --------------------\n")
+	// start to fetch the metadata from IPFS
+	startFetchingMetadata(file)
 
 	// close file
 	file.Close()
 
-	fmt.Println(time.Now().String())
+	fmt.Printf("\n@@@ End Time: %s\n\n", time.Now().String())
 }
--- metadata.go
+++ metadata.go
@@ -3,14 +3,17 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"time"
 )
 
 // send request to public IPFS gateway and receive the response
-func getMetadata(start int, count int, ch_result chan bool) {
+func getMetadata(file *os.File, start int, count int, ch_result chan bool) {
 	for i := start; i < start+count; i++ {
 		// make request url
 		url := "https://ipfs.io/ipfs/"
@@ -36,14 +39,14 @@ func getMetadata(start int, count int, ch_result chan bool) {
 		}
 
 		// write metadata
-		writeMetadata(body)
+		writeMetadata(file, body)
 	}
 
 	ch_result <- true
 }
 
 // write the metadata to file
-func writeMetadata(data []byte) {
+func writeMetadata(file *os.File, data []byte) {
 	// parse the data
 	var ret interface{}
 	json.Unmarshal(data, &ret)
@@ -53,9 +56,53 @@ func writeMetadata(data []byte) {
 	enc := json.NewEncoder(&buf)
 	enc.SetIndent("", "  ")
 	enc.Encode(ret)
+	buf.WriteString(",")
 	_, err := file.Write(buf.Bytes())
 
-	if err == nil {
-		// fmt.Printf("%+v\n", ret)
+	if err != nil {
+		fmt.Println("Error occured on writing metadata.txt:", err.Error())
+	} else {
+		fmt.Print(">")
 	}
+}
+
+func startFetchingMetadata(file *os.File) {
+	// initialize the variables
+	total_cnt := config.Ipfs_info.Total_Count
+	thread_cnt := config.Ipfs_info.Thread_Count
+	ch_result := make(chan bool, thread_cnt)
+
+	file.WriteString("[\n")
+
+	// fetch the metadata from IPFS
+	fmt.Println("\nStarting the fetching metadata from IPFS now")
+	fmt.Println("-------------------- " + time.Now().String() + " --------------------")
+
+	// generate multithread for requesting the metadata from IPFS
+	req_cnt := total_cnt / thread_cnt
+	for i := 0; i < thread_cnt; i++ {
+		if i < thread_cnt-1 {
+			go getMetadata(file, config.Ipfs_info.Start_Index+i*10, req_cnt, ch_result)
+		} else {
+			go getMetadata(file, config.Ipfs_info.Start_Index+i*10, req_cnt+(total_cnt%thread_cnt), ch_result)
+		}
+	}
+
+	// parse and write the metadata to file
+	for i := 0; i < thread_cnt; i++ {
+		<-ch_result
+	}
+
+	// remove the last comma(",")
+	// fInfo, err := file.Stat()
+	// if err != nil {
+	// 	fmt.Println("error occured on file.Stat():", err.Error())
+	// 	return
+	// }
+	// file.Truncate(fInfo.Size() - 1)
+
+	file.WriteString("]")
+
+	fmt.Println("\n-------------------- " + time.Now().String() + " --------------------")
+	fmt.Printf("Finished fetching metadata from IPFS\n\n")
 }
